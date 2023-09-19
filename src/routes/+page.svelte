b303e245a0c4015a0dfd9a7adf6feedf350a884b
Thiago Marçal <thiagormarcal@gmail.com>
2023-09-19 09:06:28 -0300

    - Added back and forward behavior

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,14 +9,35 @@
         return await invoke("list_files", {path})
     }
 
-    const openEntry = async (entry) => {
-        console.log(`clicked: ${entry.name}`)
-        if (entry.is_dir) {
-            inputValue = entry.name
+    const goBack = async () => {
+        if (inputStackBack && inputStackBack.length > 1) {
+            inputStackForward.push(inputStackBack.pop())
+            console.log(`stack back: ${inputStackBack}`)
+
+            console.log(`search for on back: ${inputStackBack}`)
+            inputValue = inputStackBack[inputStackBack.length - 1];
             await doSearch()
         }
     }
 
+    const goForward = async () => {
+        if (inputStackForward && inputStackForward.length) {
+            let current = inputStackForward.pop()
+            inputStackBack.push(current)
+            inputValue = current
+            await doSearch()
+        }
+    }
+
+    const openEntry = async (entry) => {
+        console.log(`clicked: ${entry.name}`)
+        if (entry.is_dir) {
+            inputValue = entry.name
+            inputStackBack.push(inputValue)
+            await doSearch()
+            console.log(`stack: ${inputStackBack}`)
+        }
+    }
 
     type Entry = {
         name: string;
@@ -32,11 +53,16 @@
         return d.toDateString()
     }
 
+    async function search() {
+        inputStackBack.push(inputValue)
+        await doSearch()
+    }
 
     async function doSearch() {
         loading = true
         let result = await listFiles(inputValue)
         console.log(result);
+
         entries = result
             .filter(item => Boolean(item.Ok))
             .map(item => item.Ok)
@@ -47,10 +73,13 @@
                 created: parseDate(item.created.secs_since_epoch),
                 modified: parseDate(item.modified.secs_since_epoch)
             }))
+
         loading = false
     }
 
     let entries: Entry[] = [];
+    let inputStackBack: string[] = []
+    let inputStackForward: string[] = []
 
     let inputValue = ""
     let loading = false
@@ -63,12 +92,12 @@
         <div class="flex flex-row flex-grow gap-3 p-3 h-full justify-center items-center">
             <div class="flex flex-row gap-3 basis-1/12 justify-center">
                 <div>
-                    <button type="button" class="btn-icon variant-filled">
+                    <button type="button" on:click={goBack} class="btn-icon variant-filled">
                         <i class="fa-solid fa-arrow-left"></i>
                     </button>
                 </div>
                 <div>
-                    <button type="button" class="btn-icon variant-filled">
+                    <button type="button" on:click={goForward} class="btn-icon variant-filled">
                         <i class="fa-solid fa-arrow-right"></i>
                     </button>
                 </div>
@@ -78,7 +107,7 @@
                        placeholder="Search...">
             </div>
             <div class="flex flex-row gap-3 basis-1/12 justify-center">
-                <button type="button" class="btn-icon variant-filled" on:click={doSearch}>
+                <button type="button" class="btn-icon variant-filled" on:click={search}>
                     <i class="fa-solid fa-skull"></i>
                 </button>
             </div>
@@ -87,12 +116,12 @@
 
         {#if loading}
             <div>
-                <ProgressBar />
+                <ProgressBar/>
             </div>
         {/if}
 
     </div>
-    <div class="card basis-3/4 p-4 flex flex-row" >
+    <div class="card basis-3/4 p-4 flex flex-row">
         <div class="table-container max-h-screen overflow-y-auto">
             <!-- Native Table Element -->
             <table class="table table-hover table-fixed w-full">
